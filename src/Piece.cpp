38700b26a217b3de3db6a6bd3d91616ce841ca80
Ondrej Majer <on.majer@gmail.com>
2019-07-26 04:18:28 +0200

    improved move function

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -41,38 +41,21 @@ void Piece::rookMoves(const char y, const int x, Board& board, vector< pair<char
 void Piece::bishopMoves(const char y, const int x, Board& board, vector< pair<char, int> >& moves) const{
     char i = y;
     int j = x;
-    while(j > 1 && i > 'a' && (board.at(i-1, j-1) == NULL || board.at(i-1, j-1)->getColor() != color) ){
-        j--;
-        i--;
-        moves.push_back(make_pair(i, j));
-        if(board.at(i, j) != NULL && board.at(i, j)->getColor() != color)
-            break;
+
+    int iter[4][2] = {{-1, -1}, {1, 1}, {-1, 1}, {1, -1}};
+
+    for(int k = 0; k < 4; k++){
+        while(j >= 1 && i >= 'a' && j <= 8 && i <= 'h' && (board.at(i+iter[k][0], j+iter[k][1]) == NULL || board.at(i+iter[k][0], j+iter[k][1])->getColor() != color) ){
+            i+=iter[k][0];
+            j+=iter[k][1];
+            if(j >= 1 && i >= 'a' && j <= 8 && i <= 'h'){
+                moves.push_back(make_pair(i, j));
+            }
+            if(board.at(i, j) != NULL && board.at(i, j)->getColor() != color)
+                break;
+        }
+        j = x;
+        i = y;
     }
-    j = x;
-    i = y;
-    while(j < 8 && i < 'h' && (board.at(i+1, j+1) == NULL || board.at(i+1, j+1)->getColor() != color) ){
-        j++;
-        i++;
-        moves.push_back(make_pair(i, j));
-        if(board.at(i, j) != NULL && board.at(i, j)->getColor() != color)
-            break;
-    }
-    j = x;
-    i = y;
-    while(i > 'a' && j < 8 && (board.at(i-1, j+1) == NULL || board.at(i-1, j+1)->getColor() != color) ){
-        i--;
-        j++;
-        moves.push_back(make_pair(i, j));
-        if(board.at(i, j) != NULL && board.at(i, j)->getColor() != color)
-            break;
-    }
-    j = x;
-    i = y;
-    while(i < 'h' && j > 1 && (board.at(i+1, j-1) == NULL || board.at(i+1, j-1)->getColor() != color) ){
-        i++;
-        j--;
-        moves.push_back(make_pair(i, j));
-        if(board.at(i, j) != NULL && board.at(i, j)->getColor() != color)
-            break;
-    }
+
 }
